a1f093db64409a331df8effc14a0ee188b1d3e68
cano.dedan20 <danielcdc8591@gmail.com>
2020-10-02 09:53:34 +0200

    Tercer commit

--- Exercise/src/main/java/com/salesianos/to_do_list/Exercise/controller/MainController.java
+++ Exercise/src/main/java/com/salesianos/to_do_list/Exercise/controller/MainController.java
@@ -1,8 +1,17 @@
 package com.salesianos.to_do_list.Exercise.controller;
 
+import com.salesianos.to_do_list.Exercise.services.TaskService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class MainController {
+
+    private final TaskService taskService;
+
 
 }
